4ac7a989191fb2118aa6ff73eb517f59b8f8ccc3
Vladimir Dimitrov <vladimir.d@sbtech.com>
2019-08-19 16:02:48 +0300

    moving monsters to external files

---
+++ WhateverRPGEngine/Models/ItemPercentage.cs
@@ -0,0 +1,14 @@
+﻿namespace WhateverRPGEngine.Models
+{
+    public class ItemPercentage
+    {
+        public int ID { get; }
+        public int Percentage { get; }
+
+        public ItemPercentage(int id, int percentage)
+        {
+            ID = id;
+            Percentage = percentage;
+        }
+    }
+}
--- WhateverRPGEngine/Models/Monster.cs
+++ WhateverRPGEngine/Models/Monster.cs
@@ -1,21 +1,57 @@
 ﻿namespace WhateverRPGEngine.Models
 {
-    using System.Collections.ObjectModel;
+    using System.Collections.Generic;
+    using WhateverRPGEngine.Factories;
     using WhateverRPGEngine.Utils;
 
     public class Monster : LivingEntity
     {
+        private readonly List<ItemPercentage> _lootTable =
+            new List<ItemPercentage>();
+
+        public int ID { get; }
+
         public string ImageName { get; }
 
         public int RewardExperiencePoints { get; }
 
-        public Monster(string name, string imageName,
-                int maximumHitPoints, int currentHitPoints,
-                int rewardExperiencePoints, int gold) :
-            base(name, maximumHitPoints, currentHitPoints, gold)
+        public Monster(int id, string name, string imageName,
+                int maximumHitPoints,
+                GameItem currentWeapon,int rewardExperiencePoints, int gold) :
+            base(name, maximumHitPoints, maximumHitPoints, gold)
         {
+            ID = id;
             ImageName = $"/WhateverRPGEngine;component/Resources/MonsterImageFiles/{imageName}";
+            CurrentWeapon = currentWeapon;
             RewardExperiencePoints = rewardExperiencePoints;
+        }
+
+        public void AddItemToLootTable(int id, int percentage)
+        {
+            // Remove the entry from the loot table,
+            // if it already contains an entry with this ID
+            _lootTable.RemoveAll(ip => ip.ID == id);
+
+            _lootTable.Add(new ItemPercentage(id, percentage));
+        }
+
+        public Monster GetNewInstance()
+        {
+            Monster newMonster =
+                new Monster(ID, Name, ImageName, MaximumHitPoints, CurrentWeapon,
+                            RewardExperiencePoints, Gold);
+
+            foreach (ItemPercentage itemPercentage in _lootTable)
+            {
+                newMonster.AddItemToLootTable(itemPercentage.ID, itemPercentage.Percentage);
+
+                if (RandomNumberGenerator.NumberBetween(1, 100) <= itemPercentage.Percentage)
+                {
+                    newMonster.AddItemToInventory(ItemFactory.CreateGameItem(itemPercentage.ID));
+                }
+            }
+
+            return newMonster;
         }
     }
 }
